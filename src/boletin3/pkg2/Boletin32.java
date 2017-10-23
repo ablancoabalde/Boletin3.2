c926e5ba7fe9679812b190db67987c972f26046f
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-23 10:46:39 +0200

    Boletin 3.2

---
+++ src/boletin3/pkg2/Boletin32.java
@@ -0,0 +1,15 @@
+
+package boletin3.pkg2;
+
+public class Boletin32 {
+
+    public static void main(String[] args) {
+  
+        Satelite sat1 = new Satelite();
+        Satelite sat2 = new Satelite(20,150,36);
+        sat1.verPosicion();
+        sat2.verPosicion();
+    }
+    
+    }
+    
